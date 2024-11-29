7ff7084c23d467c125e3f5e7f539a9415222dff4
Stef <sttqueries@gmail.com>
2024-11-29 21:34:03 +0000

    Automatic Minifying

--- menu-toggle.js
+++ menu-toggle.js
@@ -1,1 +1,18 @@
-const navToggle=document.getElementById('nav-toggle');const navLinks=document.getElementById('nav-links');navToggle.addEventListener('click',()=>{navLinks.classList.toggle('show')});document.querySelectorAll('a[href^="#"]').forEach(anchor=>{anchor.addEventListener('click',function(e){e.preventDefault();const targetElement=document.querySelector(this.getAttribute('href'));if(targetElement){targetElement.scrollIntoView({behavior:'smooth',})}})})
+// JavaScript for Menu Toggle
+const navToggle = document.getElementById('nav-toggle');
+const navLinks = document.getElementById('nav-links');
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
+// Smooth scrolling when clicking anchor links (if applicable)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+            });
+        }
+    });
+});
